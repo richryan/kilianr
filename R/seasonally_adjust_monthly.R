05646b0704f98e70bcbac4bdbf4daece31ac10f1
Rich Ryan <richryan@csub.edu>
2024-08-31 17:30:56 +0000

    add comment for seasonal check

--- R/seasonally_adjust_monthly.R
+++ R/seasonally_adjust_monthly.R
@@ -14,6 +14,7 @@
 #' @examples
 #' seasonally_adjust(data$some_series, data$date)
 seasonally_adjust_monthly <- function(x, date) {
+  # Check for availability of suggested package seasonal
   if (!requireNamespace("seasonal", quietly = TRUE)) {
     stop(
       "Package \"seasonal\" must be installed to use this function.",
